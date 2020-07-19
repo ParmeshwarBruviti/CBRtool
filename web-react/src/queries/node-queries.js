d06103762813d3fc08ffde11bc7141be9f31a6e9
nitin <nitin@bruviti.com>
2020-07-19 23:52:50 +0530

    Edit node Functionality

--- web-react/src/queries/node-queries.js
+++ web-react/src/queries/node-queries.js
@@ -26,6 +26,32 @@ const ADD_QUESTION_MUTATION = gql`
   }
 `
 
+const UPDATE_QUESTION_MUTATION = gql`
+  mutation(
+    $id: ID!
+    $content: String
+    $context: String
+    $hint: String
+    $raw_content: String
+    $source_ref: String
+    $space: String
+    $start: Boolean
+  ) {
+    UpdateQuestion(
+      questionId: $id
+      content: $content
+      context: $context
+      hint: $hint
+      raw_content: $raw_content
+      source_ref: $source_ref
+      space: $space
+      start: $start
+    ) {
+      _id
+    }
+  }
+`
+
 const GET_QUESTION = gql`
   query($id: ID!) {
     Question(questionId: $id) {
@@ -82,6 +108,38 @@ const ADD_SOLUTION_MUTATION = gql`
   }
 `
 
+const UPDATE_SOLUTION_MUTATION = gql`
+  mutation(
+    $id: ID!
+    $content: String
+    $context: String
+    $hint: String
+    $raw_content: String
+    $source_ref: String
+    $space: String
+    $parts: [String]
+    $attachment_types: [String]
+    $attachment_titles: [String]
+    $attachment_paths: [String]
+  ) {
+    UpdateSolution(
+      solutionId: $id
+      content: $content
+      context: $context
+      hint: $hint
+      raw_content: $raw_content
+      source_ref: $source_ref
+      space: $space
+      parts: $parts
+      attachment_types: $attachment_types
+      attachment_titles: $attachment_titles
+      attachment_paths: $attachment_paths
+    ) {
+      _id
+    }
+  }
+`
+
 const GET_SOLUTION = gql`
   query($id: ID!) {
     Solution(solutionId: $id) {
@@ -103,7 +161,7 @@ const GET_SOLUTION = gql`
 
 const DELETE_SOLUTION = gql`
   mutation($id: ID!) {
-    DeleteSolution(soultionId: $id) {
+    DeleteSolution(solutionId: $id) {
       _id
     }
   }
@@ -111,9 +169,11 @@ const DELETE_SOLUTION = gql`
 
 export {
   ADD_QUESTION_MUTATION,
+  UPDATE_QUESTION_MUTATION,
   GET_QUESTION,
   DELETE_QUESTION,
   ADD_SOLUTION_MUTATION,
+  UPDATE_SOLUTION_MUTATION,
   GET_SOLUTION,
   DELETE_SOLUTION,
 }
